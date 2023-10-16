c246cf9d2845e4308820041714c2c3a87ff03dc4
d3faulter <nvfbeck@gmail.com>
2023-10-16 22:20:46 +0200

    rettelser

--- server/db/database.js
+++ server/db/database.js
@@ -1,5 +1,7 @@
+// root/server/db/database.js
+
 const mysql = require('mysql');
-const config = require('config.json');
+const config = require('../../config.js');
 
 const connection = mysql.createConnection({
     host: config.database.host,
@@ -8,9 +10,20 @@ const connection = mysql.createConnection({
     database: config.database.name
 });
 
-connection.connect(error => {
-    if (error) throw error;
-    console.log('Database connected successfully.');
+connection.connect(err => {
+    if (err) {
+        console.error('Error connecting: ' + err.stack);
+        return;
+    }
+    console.log('Connected as id ' + connection.threadId);
 });
 
-module.exports = connection;
+// Optionally, perform a simple query to further verify the connection
+connection.query('SELECT 1 + 1 AS solution', (error, results, fields) => {
+    if (error) throw error;
+    console.log('The solution is: ', results[0].solution);
+});
+
+connection.end();
+
+// module.exports = connection;
