3fb13cf695d209f3cff9a5c99c922ed218769391
d3faulter <nvfbeck@gmail.com>
2023-10-17 15:42:00 +0200

    meremere

--- server/server.js
+++ server/server.js
@@ -76,7 +76,7 @@ app.get("/store/checkout", (req, res) => {
 // });
 
 // start server på pc
-http.listen(port, '0.0.0.0', () => {
+http.listen(port, 'localhost', () => {
   console.log('Server open on port: ' + port);
 });
 
