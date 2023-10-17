f3c921c635f4bb4b5988451a49fa24afeb7bb6e0
gustav123ze <guze22ab@student.cbs.dk>
2023-10-17 13:10:04 +0000

    fix

--- server/db/database.js
+++ server/db/database.js
@@ -12,16 +12,7 @@ const pool = createPool({
 });
 
 pool.query('select * from admins', (error, results, fields) => {
-<<<<<<< HEAD
-    return console.log(results); 
+	return console.log(results);
 });
 
 module.exports = pool.promise();
-
-
-=======
-    return console.log(results);
-});
-
-module.exports = pool.promise();
->>>>>>> 306d9aee3b5ded6a5b3929c7883df99690b4797d
