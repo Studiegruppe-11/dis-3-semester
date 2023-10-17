81d4ce2879d07d1396ce69c2742f25b02495d880
d3faulter <nvfbeck@gmail.com>
2023-10-17 16:31:25 +0200

    gg

--- server/routes/showAdminsRoute.js
+++ server/routes/showAdminsRoute.js
@@ -4,13 +4,9 @@ const express = require("express");
 const adminRoute = express.Router();
 const db = require('../db/database.js');
 
-console.log(db);
-
 adminRoute.get('/', async (req, res) => {
     try {
-        const result = await db.execute('SELECT * FROM admins');
-        console.log(result);
-        const [rows, fields] = result;
+        const [rows, fields] = await db.poolPromise.query('SELECT * FROM admins');
         res.json(rows);
     } catch (error) {
         console.error('Error:', error.message);
