4dd04cc9cb5d49e1f3e97ab1682eb990e08608cc
gustav123ze <guze22ab@student.cbs.dk>
2023-10-17 13:02:07 +0000

    yy

--- server/db/database.js
+++ server/db/database.js
@@ -1,43 +1,20 @@
-// root/server/db/database.js
+const dotenv = require('dotenv');
+dotenv.config({path: '../../.env'});
 
-const mysql = require('mysql2');
-require('dotenv').config();
+const {createPool} = require('mysql2');
 
-const config = {
-    database: {
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASS,
-        name: process.env.DB_NAME
-    }
-}
+const pool = createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
 
-console.log(config);
-
-// const config = require('../../config.js');
-// console.log(config.database);
-
-// const connection = mysql.createConnection({
-//     host: config.database.host,
-//     user: config.database.user,
-//     password: config.database.password,
-//     database: config.database.name
-// });
-
-connection.connect(err => {
-    if (err) {
-        console.error('Error connecting: ' + err.stack);
-        return;
-    }
-    console.log('Connected as id ' + connection.threadId);
 });
 
-// Optionally, perform a simple query to further verify the connection
-connection.query('SELECT 1 + 1 AS solution', (error, results, fields) => {
-    if (error) throw error;
-    console.log('The solution is: ', results[0].solution);
+pool.query('select * from admins', (error, results, fields) => {
+    return console.log(results); 
 });
 
-connection.end();
+module.exports = pool.promise();
 
-// module.exports = connection;
+
