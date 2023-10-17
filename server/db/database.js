298781c4e73ab6dc409bae09c2121d47370a597a
d3faulter <nvfbeck@gmail.com>
2023-10-17 17:05:19 +0200

    gg

--- server/db/database.js
+++ server/db/database.js
@@ -13,7 +13,8 @@ const dbConfig = {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    port: 3306
 };
 
 console.log(dbConfig.host)
