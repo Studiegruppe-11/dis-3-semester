6fcdee212d26197e521f5c955b31fefcee13e898
gustav123ze <guze22ab@student.cbs.dk>
2023-10-28 20:04:15 +0200

    ret

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -17,4 +17,46 @@ router.get('/users/customers', async (req, res) => {
     }
 });
 
+
+
+
+
+// Undersøg om login er korrekt.
+router.post("/users/login", async (req, res) => {
+    const { username, password } = req.body;
+    const pool = await connection.poolPromise;
+  
+    try {
+      // Udfør SQL-forespørgslen
+      const [rows] = await pool.query(
+        'SELECT * FROM customers WHERE username = ? AND password = ?',
+        [username, password]
+      );
+  
+      if (rows.length > 0) {
+        const user = rows[0];
+  
+        // Gem brugerens id og navn i express-session
+        req.session.userId = user.customer_id;
+        req.session.username = user.username;
+  
+        // Send svar tilbage til klienten
+        res.json({ success: true });
+      } else {
+        res.json({ error: 'Forkert brugernavn eller adgangskode' });
+      }
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ error: 'Der opstod en fejl under login.' });
+    }
+  });
+
+
+  
+
+
+
+
+
+
 module.exports = router;
