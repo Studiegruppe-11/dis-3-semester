6a544889ee681db14a52bf701f43e33c3a5e4638
d3faulter <nvfbeck@gmail.com>
2023-10-29 23:58:09 +0100

    console logs

--- client/scripts/admin.js
+++ client/scripts/admin.js
@@ -1,22 +1,33 @@
 // root/client/scripts/admin.js
-console.log('admin.js loaded');
 // Function to get a cookie by name
 function getCookie(name) {
     const value = "; " + document.cookie;
+    console.log('Cookie value:', value);  // Log the entire cookie string
     const parts = value.split("; " + name + "=");
+    console.log('Split parts:', parts);  // Log the parts after splitting
     if (parts.length == 2) return parts.pop().split(";").shift();
 }
 
 // Function to display the username
 function displayUsername() {
     const username = getCookie('username');
+    console.log('Retrieved username:', username);  // Log the retrieved username
     if (username) {
         const usernameDisplay = document.getElementById('usernameDisplay');
+        console.log('usernameDisplay element:', usernameDisplay);  // Log the usernameDisplay element
         if (usernameDisplay) {
             usernameDisplay.innerText = username;
+            console.log('Username displayed');  // Log when username is displayed
+        } else {
+            console.log('usernameDisplay element not found');  // Log if usernameDisplay element is not found
         }
+    } else {
+        console.log('Username not found in cookies');  // Log if username is not found in cookies
     }
 }
 
 // Call displayUsername on page load
-window.onload = displayUsername;
+window.onload = () => {
+    console.log('Page loaded');  // Log when page is loaded
+    displayUsername();
+};
