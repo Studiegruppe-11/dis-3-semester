f6ccbd2cd005e932f2907412f473975e55b93cd0
d3faulter <nvfbeck@gmail.com>
2023-10-29 23:49:45 +0100

    frontend

---
+++ client/scripts/admin.js
@@ -0,0 +1,22 @@
+// root/client/scripts/admin.js
+
+// Function to get a cookie by name
+function getCookie(name) {
+    const value = "; " + document.cookie;
+    const parts = value.split("; " + name + "=");
+    if (parts.length == 2) return parts.pop().split(";").shift();
+}
+
+// Function to display the username
+function displayUsername() {
+    const username = getCookie('username');
+    if (username) {
+        const usernameDisplay = document.getElementById('usernameDisplay');
+        if (usernameDisplay) {
+            usernameDisplay.innerText = username;
+        }
+    }
+}
+
+// Call displayUsername on page load
+window.onload = displayUsername;
