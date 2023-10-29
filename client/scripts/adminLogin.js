68cacc73e3a1ea68d2285a157436ef06a6277242
gustav123ze <guze22ab@student.cbs.dk>
2023-10-29 23:18:41 +0100

    ret

--- client/scripts/adminLogin.js
+++ client/scripts/adminLogin.js
@@ -1,20 +1,29 @@
 // root/client/scripts/adminLogin.js
-document.getElementById('login').addEventListener('click', async () => {
-    try {
-        const username = document.getElementById('username').value;
-        const password = document.getElementById('password').value;
-        
-        // Log user input
-        console.log('Username:', username);
-        console.log('Password:', password);
-        
-        const response = await axios.post('/admin/login', { username, password });
-        if (response.data.error) {
-            document.getElementById('message').innerText = response.data.error;
-        } else {
-            window.location.href = '/admin';  // Redirect to the admin page on successful login
-        }
-    } catch (error) {
-        console.error(error);
+ocument.getElementById("login").addEventListener("click", async function () {
+    // henter username og password fra input felterne og gemmer dem i variablerne username og password.
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+
+    // sender username og password til at kunne bruges i controller. 
+    const response = await fetch('/admin/login', {
+        // metoden er post fordi vi sender data til serveren.
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            username,
+            password
+        })
+    });
+
+    const result = await response.json();
+    // hvis brugeren er logget ind, så sendes brugeren til index.html
+    if (result.success) {
+        window.location.href = "../admin";
+    } else {
+        // hvis brugeren ikke har skrevet rigtigt, så sendes en fejlbesked.
+        const errorMessage = result.error || "Forkert brugernavn eller adgangskode";
+        alert(errorMessage);
     }
 });
--- server/routes/adminRoute.js
+++ server/routes/adminRoute.js
@@ -4,40 +4,66 @@ const router = express.Router();
 const connection = require('../db/database1.js');
 
 // Middleware til at tjekke om brugeren er logget ind som admin
-function isAdmin(req, res, next) {
-    if (req.session.isAdmin) {
-        next();
-    } else {
-        res.redirect('/admin/login');
-    }
-}
+// function isAdmin(req, res, next) {
+//     if (req.session.isAdmin) {
+//         next();
+//     } else {
+//         res.redirect('/admin/login');
+//     }
+// }
 
 // Admin Login Route
 router.post('/admin/login', async (req, res) => {
     const { username, password } = req.body;
+    const pool = await connection.poolPromise;
     try {
-        const pool = await connection.poolPromise;
-        
-        // Log entire admins table (not recommended)
-        const [allAdmins] = await pool.query('SELECT * FROM admins');
-        console.log('All admins:', allAdmins);
-        
-        const [rows] = await pool.query('SELECT * FROM admins WHERE username = ? AND password = SHA2(?, 256)', [username, password]);
-        if (rows.length > 0) {
-            req.session.isAdmin = true;
-            req.session.username = username;
-            res.redirect('/admin');
-        } else {
-            res.status(401).json({ error: 'Invalid credentials' });
-        }
+      // Udfør SQL-forespørgslen
+      const [rows] = await pool.query(
+        'SELECT * FROM admins WHERE username = ? AND password = ?',
+        [username, password]
+      );
+  
+      if (rows.length > 0) {
+        const user = rows[0];
+  
+        // Gem brugerens id og navn i express-session
+        req.session.userId = user.id;
+        req.session.name = user.username;
+  
+        // Send svar tilbage til klienten
+        res.json({ success: true });
+      } else {
+        res.json({ error: 'Forkert brugernavn eller adgangskode' });
+      }
     } catch (error) {
-        console.log(error);
-        res.status(500).json({ error: error.message });
+      console.log(error);
+      res.status(500).json({ error: 'Der opstod en fejl under login.' });
     }
-});
+  });
+
+
 
 // router.get('/admin', isAdmin, (req, res) => {
 //     res.sendFile(path.join(__dirname, "../../client/pages/admin.html"));
 //   });
+
+
+
+
+router.get('/admin/admins', async (req, res) => {
+    try {
+        const pool = await connection.poolPromise;
+
+        // Udfør SQL-forespørgslen her
+        const [rows] = await pool.query('SELECT * FROM admins');
+
+        res.send(rows);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(error.message);
+    }
+});
+
+
 
 module.exports = router;
