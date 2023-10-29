eff5363fa3d42705c82a4d74557671429d9ecc3b
d3faulter <nvfbeck@gmail.com>
2023-10-29 23:49:45 +0100

    frontend

--- client/scripts/adminLogin.js
+++ client/scripts/adminLogin.js
@@ -1,25 +1,4 @@
 // root/client/scripts/adminLogin.js
-
-// Få username fra cookie
-function getCookie(name) {
-    const value = "; " + document.cookie;
-    const parts = value.split("; " + name + "=");
-    if (parts.length == 2) return parts.pop().split(";").shift();
-}
-
-// Viser username i admin.html
-function displayUsername() {
-    const username = getCookie('username');
-    if (username) {
-        const usernameDisplay = document.getElementById('usernameDisplay');
-        if (usernameDisplay) {
-            usernameDisplay.innerText = username;
-        }
-    }
-}
-
-displayUsername();
-
 document.getElementById('login').addEventListener('click', async () => {
     try {
         const username = document.getElementById('username').value;
@@ -28,11 +7,15 @@ document.getElementById('login').addEventListener('click', async () => {
         if (response.data.error) {
             document.getElementById('message').innerText = response.data.error;
         } else {
-            document.cookie = `username=${response.data.username}`;  // Set the username cookie
-            displayUsername();  // Update the DOM to display the username
+            // Update the DOM to display the username
+            const usernameDisplay = document.createElement('li');
+            usernameDisplay.innerText = response.data.username;
+            document.querySelector('ul').appendChild(usernameDisplay);
+            
             window.location.href = '/admin';  // Redirect to the admin page on successful login
         }
     } catch (error) {
         console.error(error);
     }
 });
+
