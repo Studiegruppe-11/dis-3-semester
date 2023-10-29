fda3e3c73341c09929d4a9e6432fbc5f4dc42f36
d3faulter <nvfbeck@gmail.com>
2023-10-29 22:37:29 +0100

    Admin login v2

--- server/routes/adminRoute.js
+++ server/routes/adminRoute.js
@@ -1,7 +1,35 @@
 // root/server/routes/adminRoute.js
-
 const express = require('express');
 const router = express.Router();
+const connection = require('../db/database1.js');
+
+// Middleware til at tjekke om brugeren er logget ind som admin
+function isAdmin(req, res, next) {
+    if (req.session.isAdmin) {
+        next();
+    } else {
+        res.redirect('/admin/login');
+    }
+}
+
+// Admin Login Route
+router.post('/admin/login', async (req, res) => {
+    const { username, password } = req.body;
+    try {
+        const pool = await connection.poolPromise;
+        const [rows] = await pool.query('SELECT * FROM admins WHERE username = ? AND password = SHA2(?, 256)', [username, password]);
+        if (rows.length > 0) {
+            req.session.isAdmin = true;
+            req.session.username = username;
+            res.redirect('/admin');
+        } else {
+            res.status(401).json({ error: 'Invalid credentials' });
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: error.message });
+    }
+});
 
 
 module.exports = router;
