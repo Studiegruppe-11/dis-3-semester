bcf1e716b7040720aeb2d82d2ef589cc1eab844b
gustav123ze <guze22ab@student.cbs.dk>
2023-11-03 19:02:06 +0100

    Update twilio.js

--- server/utility/twilio.js
+++ server/utility/twilio.js
@@ -2,12 +2,11 @@ const path = require('path');
 const dotenv = require('dotenv');
 dotenv.config({ path: path.resolve(__dirname, '../../.env') });
 
-
 const twilio = {
-myphone: process.env.MY_PHONE,
-twilioPhone: process.env.TWILIO_PHONE,
-accountSid: process.env.TWILIO_SID,
-authToken: process.env.TWILIO_TOKEN
+  myphone: process.env.MY_PHONE,
+  twilioPhone: process.env.TWILIO_PHONE,
+  accountSid: process.env.TWILIO_SID,
+  authToken: process.env.TWILIO_TOKEN
 };
 
 const accountSid = twilio.accountSid;
@@ -15,12 +14,10 @@ const authToken = twilio.authToken;
 const client = require('twilio')(accountSid, authToken);
 
 client.messages
-    .create({
-        body: 'Test',
-        from: twilio.twilioPhone,
-        to: twilio.myphone
-    })
-    .then(message => console.log(message.sid))
-    .done();
-
-
+  .create({
+    body: 'Dette er en testbesked',
+    from: twilio.twilioPhone,
+    to: twilio.myphone
+  })
+  .then(message => console.log(`Besked sendt med SID: ${message.sid}`))
+  .catch(error => console.error(`Fejl: ${error.message}`));
