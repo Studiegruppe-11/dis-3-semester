93aeaf4c0a4efabb6b4278bbc1e1c2e6bb0a6d11
gustav123ze <guze22ab@student.cbs.dk>
2023-11-03 19:54:35 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 // root/server/server.js
 const express = require("express");
+const bodyParser = require('body-parser');
 const cors = require("cors");
 const path = require("path");
 const app = express();
