c95002e44edb752870a29c59da2e6c0247f13adb
gustav123ze <guze22ab@student.cbs.dk>
2023-11-06 23:43:57 +0100

    css

--- client/scripts/bestil.js
+++ client/scripts/bestil.js
@@ -9,30 +9,57 @@ document.addEventListener('DOMContentLoaded', async () => {
         let sandwichList = document.getElementById("sandwichList");
         let juiceList = document.getElementById("juiceList");
 
+        // Opret separate kasser for hver sandwich
         sandwich.forEach((sandwich) => {
-            let li = document.createElement("li");
-            li.innerHTML = `${sandwich.name} ${sandwich.description} ${sandwich.price} kr`;
-            sandwichList.appendChild(li);
-        }
+            let menuItem = document.createElement("div");
+            menuItem.classList.add("menu-item");
 
-        );
+            let img = document.createElement("img");
+            img.src = sandwich.imageURL;
+            menuItem.appendChild(img);
 
+            let name = document.createElement("p");
+            name.innerHTML = `Name: ${sandwich.name}`;
+            menuItem.appendChild(name);
+
+            let description = document.createElement("p");
+            description.innerHTML = `Description: ${sandwich.description}`;
+            menuItem.appendChild(description);
+
+            let price = document.createElement("p");
+            price.innerHTML = `Price: ${sandwich.price} kr`;
+            menuItem.appendChild(price);
+
+            sandwichList.appendChild(menuItem);
+        });
+
+        // Opret separate kasser for hver juice
         juice.forEach((juice) => {
-            let li = document.createElement("li");
-            li.innerHTML = `${juice.name} ${juice.description} ${juice.price} kr`;
-            juiceList.appendChild(li);
+            let menuItem = document.createElement("div");
+            menuItem.classList.add("menu-item");
 
-        }
+            let img = document.createElement("img");
+            img.src = juice.imageURL;
+            menuItem.appendChild(img);
 
-        );
+            let name = document.createElement("p");
+            name.innerHTML = `Name: ${juice.name}`;
+            menuItem.appendChild(name);
+
+            let description = document.createElement("p");
+            description.innerHTML = `Description: ${juice.description}`;
+            menuItem.appendChild(description);
+
+            let price = document.createElement("p");
+            price.innerHTML = `Price: ${juice.price} kr`;
+            menuItem.appendChild(price);
+
+            juiceList.appendChild(menuItem);
+        });
 
     } catch (error) {
 
         console.log(error);
 
     }
-
-
-      });
-
-
+});
