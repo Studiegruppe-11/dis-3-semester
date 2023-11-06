c97cf0faf69b99c3c03cb95703575f5b3c092f92
gustav123ze <guze22ab@student.cbs.dk>
2023-11-06 23:21:32 +0100

    ret

--- client/scripts/bestil.js
+++ client/scripts/bestil.js
@@ -1,5 +1,24 @@
 document.addEventListener('DOMContentLoaded', async () => {
 
-    console.log("DOM loaded and parsed");
+    try {
+        const response = await fetch("/orders/sandwich");
+        const sandwich = await response.json();
+        const response2 = await fetch("/orders/juice");
+        const juice = await response2.json();
 
-});
+        console.log(sandwich, juice);
+
+
+        
+          //document.getElementById("user").innerHTML = result.name;
+        
+    
+      } catch (error) {
+          console.log(error);
+          
+        }
+
+
+      });
+
+
