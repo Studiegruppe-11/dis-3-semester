70cf0be5c51ba02e362c741ad0e04631741b5fe9
gustav123ze <guze22ab@student.cbs.dk>
2023-11-06 23:39:57 +0100

    Update bestil.js

--- client/scripts/bestil.js
+++ client/scripts/bestil.js
@@ -6,8 +6,6 @@ document.addEventListener('DOMContentLoaded', async () => {
         const response2 = await fetch("/orders/juice");
         const juice = await response2.json();
 
-        console.log(sandwich, juice);
-
         let sandwichList = document.getElementById("sandwichList");
         let juiceList = document.getElementById("juiceList");
 
