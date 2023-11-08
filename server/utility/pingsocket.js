90d7760b0f56e406e43cfc7a107c7a2361688772
d3faulter <nvfbeck@gmail.com>
2023-11-08 11:59:34 +0100

    Refactor pingSocket import and function name

--- server/utility/pingsocket.js
+++ server/utility/pingsocket.js
@@ -1,18 +1,30 @@
 const ping = require('ping');
+const socketIO = require('socket.io');
 
-exports.setupPing = (socket) => {
-  const measurePing = async () => {
-    try {
-      const target = '164.90.228.42';
-      const res = await ping.promise.probe(target);
-      const pingTime = res.time;
-      socket.emit('pingUpdate', { ping: pingTime });
-      socket.emit('rttUpdate', { rtt: pingTime * 2 });
-    } catch (err) {
-      console.error('Ping error:', err);
-    }
-  };
+function setupPing(server) {
+  const io = socketIO(server);
 
-  measurePing();
-  setInterval(measurePing, 600000); //every 10 minutes
-};
+  io.on('connection', (socket) => {
+    console.log('En klient er tilsluttet via socket.');
+
+    // Funktion til at måle ping og RTT
+    const measurePing = async () => {
+      try {
+        const target = '164.90.228.42'; // Erstat med din servers IP
+        const res = await ping.promise.probe(target);
+        const pingTime = res.time;
+        socket.emit('pingUpdate', { ping: pingTime });
+        // RTT beregnes som dobbelt ping-tid, da det er en round-trip
+        socket.emit('rttUpdate', { rtt: pingTime * 2 });
+      } catch (err) {
+        console.error('Ping error:', err);
+      }
+    };
+  
+    // Mål ping og RTT ved forbindelse og derefter hvert minut.
+    measurePing();
+    setInterval(measurePing, 600000); //hver 10. min.
+  });
+}
+
+module.exports = setupPing;
