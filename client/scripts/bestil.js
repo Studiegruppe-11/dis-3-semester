c9a11e9d8e82685e2f88e7b75aed125d4fc92d6c
gustav123ze <guze22ab@student.cbs.dk>
2023-11-23 11:24:35 +0100

    vis navn i bestil

--- client/scripts/bestil.js
+++ client/scripts/bestil.js
@@ -75,3 +75,22 @@ document.addEventListener('DOMContentLoaded', async () => {
 console.log("Bestil.js loaded");
 
 
+
+window.addEventListener("DOMContentLoaded", async () => {
+    try {
+      const response = await fetch("/users/show");
+      const result = await response.json();
+  
+      if (result.userId && result.name) {
+        // Viser navn hvis man er logget ind
+        document.getElementById("user").innerHTML = result.name;
+        // Skal ikke vise "opret bruger" hvis man er logget ind.
+      }
+    } catch (error) {
+        console.log(error);
+        // Håndter fejlhåndtering her
+      }
+    });
+
+
+
