de485b7e6d2f87ad925628f24a95ed216f1d4f98
gustav123ze <guze22ab@student.cbs.dk>
2023-11-23 11:37:24 +0100

    ret

--- client/scripts/admin.js
+++ client/scripts/admin.js
@@ -7,37 +7,66 @@
 // virkelig meget kode for at vise navn. kan meget lettere gøres som i home.js ved bare at bruge express session. 
 
 
-function getCookie(name) {
-    const value = "; " + document.cookie;
-   // console.log('Cookie value:', value);  // Log the entire cookie string
-    const parts = value.split("; " + name + "=");
-   // console.log('Split parts:', parts);  // Log the parts after splitting
-    if (parts.length == 2) return parts.pop().split(";").shift();
-}
+// function getCookie(name) {
+//     const value = "; " + document.cookie;
+//    // console.log('Cookie value:', value);  // Log the entire cookie string
+//     const parts = value.split("; " + name + "=");
+//    // console.log('Split parts:', parts);  // Log the parts after splitting
+//     if (parts.length == 2) return parts.pop().split(";").shift();
+// }
 
-// Function to display the username
-function displayUsername() {
-    const username = getCookie('username');
-  //  console.log('Retrieved username:', username);  // Log the retrieved username
-    if (username) {
-        const usernameDisplay = document.getElementById('usernameDisplay');
-        //console.log('usernameDisplay element:', usernameDisplay);  // Log the usernameDisplay element
-        if (usernameDisplay) {
-            usernameDisplay.innerText = username;
-            //console.log('Username displayed');  // Log when username is displayed
-        } else {
-            //console.log('usernameDisplay element not found');  // Log if usernameDisplay element is not found
-        }
-    } else {
-     //   console.log('Username not found in cookies');  // Log if username is not found in cookies
+// // Function to display the username
+// function displayUsername() {
+//     const username = getCookie('username');
+//   //  console.log('Retrieved username:', username);  // Log the retrieved username
+//     if (username) {
+//         const usernameDisplay = document.getElementById('usernameDisplay');
+//         //console.log('usernameDisplay element:', usernameDisplay);  // Log the usernameDisplay element
+//         if (usernameDisplay) {
+//             usernameDisplay.innerText = username;
+//             //console.log('Username displayed');  // Log when username is displayed
+//         } else {
+//             //console.log('usernameDisplay element not found');  // Log if usernameDisplay element is not found
+//         }
+//     } else {
+//      //   console.log('Username not found in cookies');  // Log if username is not found in cookies
+//     }
+// }
+
+// // Call displayUsername on page load
+// window.onload = () => {
+//  //   console.log('Page loaded');  // Log when page is loaded
+//     displayUsername();
+// };
+
+
+
+
+
+window.addEventListener("DOMContentLoaded", async () => {
+  try {
+    const response = await fetch("/admins/show");
+    const result = await response.json();
+
+    if (result.userId && result.name) {
+      // Viser navn hvis man er logget ind
+      document.getElementById("usernameDisplay").innerHTML = result.name;
+      // Skal ikke vise "opret bruger" hvis man er logget ind.
     }
-}
+  } catch (error) {
+      console.log(error);
+      // Håndter fejlhåndtering her
+    }
+  });
 
-// Call displayUsername on page load
-window.onload = () => {
- //   console.log('Page loaded');  // Log when page is loaded
-    displayUsername();
-};
+
+
+
+
+
+
+
+
 
 
 
@@ -190,5 +219,11 @@ if (data.rtt > 1000) {
   
 
 // };
+
+
+
+
+
+
  
  
