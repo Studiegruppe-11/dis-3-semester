2382313dc1c1a18dda3bc53d5c0a0cc8486d2953
gustav123ze <guze22ab@student.cbs.dk>
2023-11-25 14:11:59 +0100

    Update admin.js

--- client/scripts/admin.js
+++ client/scripts/admin.js
@@ -63,38 +63,38 @@ function updatePlacedOrders(placedOrders) {
 
 
 
-// SOCKET PING START
-// socket til at vise rtt og ping i real time på admin siden.
-// Opret en WebSocket-forbindelse til serveren
-const socket = io();
+// // SOCKET PING START
+// // socket til at vise rtt og ping i real time på admin siden.
+// // Opret en WebSocket-forbindelse til serveren
+// const socket = io();
 
-// Lyt efter opdateringer fra serveren
+// // Lyt efter opdateringer fra serveren
 
-socket.on('rttUpdate', (data) => {
-    // Opdater HTML-elementet med RTT-oplysninger
-    document.getElementById('rttInfo').textContent = `RTT: ${data.rtt} ms`;
+// socket.on('rttUpdate', (data) => {
+//     // Opdater HTML-elementet med RTT-oplysninger
+//     document.getElementById('rttInfo').textContent = `RTT: ${data.rtt} ms`;
 
-if (data.rtt > 1000) {
-  document.getElementById('rttInfo').style.color = 'red';
+// if (data.rtt > 1000) {
+//   document.getElementById('rttInfo').style.color = 'red';
 
-} else {
-  // Hvis RTT er under grænsen, sæt farven til grøn eller orange som tidligere
-  if (data.rtt < 1000) {
-    document.getElementById('rttInfo').style.color = 'green';
-} 
-}
-  });
-  socket.on('pingUpdate', (data) => {
-    // Opdater HTML-elementet med ping-oplysninger
-    document.getElementById('pingInfo').textContent = `Ping: ${data.ping} ms`;
+// } else {
+//   // Hvis RTT er under grænsen, sæt farven til grøn eller orange som tidligere
+//   if (data.rtt < 1000) {
+//     document.getElementById('rttInfo').style.color = 'green';
+// } 
+// }
+//   });
+//   socket.on('pingUpdate', (data) => {
+//     // Opdater HTML-elementet med ping-oplysninger
+//     document.getElementById('pingInfo').textContent = `Ping: ${data.ping} ms`;
   
-  if(data.ping < 500) {
-      document.getElementById('pingInfo').style.color = 'green';
-  } else if (data.ping > 500) {
-      document.getElementById('pingInfo').style.color = 'orange';
-  }
+//   if(data.ping < 500) {
+//       document.getElementById('pingInfo').style.color = 'green';
+//   } else if (data.ping > 500) {
+//       document.getElementById('pingInfo').style.color = 'orange';
+//   }
   
-  });
+//   });
 
-// SOCKET PING SLUT 
+// // SOCKET PING SLUT 
  
