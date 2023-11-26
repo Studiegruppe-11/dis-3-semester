3feaa1344a58bc3e35fb5ac2c4cd714b5f93bb2a
gustav123ze <guze22ab@student.cbs.dk>
2023-11-26 15:42:33 +0100

    Update adminOrder.route.js

--- server/routes/adminOrder.route.js
+++ server/routes/adminOrder.route.js
@@ -107,7 +107,7 @@ router.post('/updatestatus', async (req, res) => {
             UPDATE placedorders
             SET status = "finished"
             WHERE placedorders_id = ?
-        `, [82]);
+        `, [81]);
         //[req.body.placedorders_id]); 
 
         res.send({ status: "success" });
