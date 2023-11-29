76ea96c096d6c775bce2cfd4cc097e20b7406436
Nicolas V. F. B. <nvfbeck@gmail.com>
2023-11-29 16:00:33 +0100

    Refactor image upload route to handle multiple file uploads

--- server/routes/imageRoute.js
+++ server/routes/imageRoute.js
@@ -4,22 +4,27 @@ const router = express.Router();
 const { uploadImage } = require('../utility/cloudinaryUtility.js'); // Utility module for Cloudinary
 
 router.post('/upload/images', async (req, res) => {
-  try {
-    let uploadPromises = [];
-    // Check if multiple files are uploaded
-    if (req.files.image.length) {
-      uploadPromises = req.files.image.map(file => uploadImage(file.tempFilePath));
-    } else {
-      // Single file upload
-      uploadPromises = [uploadImage(req.files.image.tempFilePath)];
+    try {
+      // Check if file data is present
+      if (!req.files || !req.files.image) {
+        return res.status(400).send('No files were uploaded.');
+      }
+  
+      let uploadPromises = [];
+      // Check if multiple files are uploaded
+      if (Array.isArray(req.files.image)) {
+        uploadPromises = req.files.image.map(file => uploadImage(file.tempFilePath));
+      } else {
+        // Single file upload
+        uploadPromises = [uploadImage(req.files.image.tempFilePath)];
+      }
+  
+      const results = await Promise.all(uploadPromises);
+      res.status(200).json({ message: 'Images uploaded successfully', urls: results });
+    } catch (error) {
+      console.error('Error uploading images:', error.message, error.stack);
+      res.status(500).send('Error uploading images');
     }
-
-    const results = await Promise.all(uploadPromises);
-    res.status(200).json({ message: 'Images uploaded successfully', urls: results });
-  } catch (error) {
-    console.error('Error uploading images:', error, error.msg, error.stack);
-    res.status(500).send('Error uploading images');
-  }
-});
+  });
 
 module.exports = router;
