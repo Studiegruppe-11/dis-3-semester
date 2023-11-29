d891ae6d49df399267f61abb507fb469b8c6ba4a
Nicolas V. F. B. <nvfbeck@gmail.com>
2023-11-29 14:31:22 +0100

    Commented out image upload functionality

--- client/scripts/upload.js
+++ client/scripts/upload.js
@@ -1,19 +1,19 @@
-document.getElementById('uploadForm').addEventListener('submit', function(e) {
-    e.preventDefault();
+// document.getElementById('uploadForm').addEventListener('submit', function(e) {
+//     e.preventDefault();
 
-    const formData = new FormData();
-    formData.append('image', e.target.image.files[0]);
+//     const formData = new FormData();
+//     formData.append('image', e.target.image.files[0]);
 
-    fetch('/images/upload', {
-        method: 'POST',
-        body: formData
-    })
-    .then(response => response.json())
-    .then(data => {
-        document.getElementById('uploadStatus').innerHTML = `Upload successful! Image URL: <a href="${data.url}">${data.url}</a>`;
-    })
-    .catch(error => {
-        console.error('Error:', error);
-        document.getElementById('uploadStatus').textContent = 'Upload failed';
-    });
-});
+//     fetch('/images/upload', {
+//         method: 'POST',
+//         body: formData
+//     })
+//     .then(response => response.json())
+//     .then(data => {
+//         document.getElementById('uploadStatus').innerHTML = `Upload successful! Image URL: <a href="${data.url}">${data.url}</a>`;
+//     })
+//     .catch(error => {
+//         console.error('Error:', error);
+//         document.getElementById('uploadStatus').textContent = 'Upload failed';
+//     });
+// });
--- server/utility/imageUtility.js
+++ server/utility/imageUtility.js
@@ -1,13 +1,13 @@
-// cloudinaryUtil.js
-const cloudinary = require('cloudinary').v2;
+// // cloudinaryUtil.js
+// const cloudinary = require('cloudinary').v2;
 
-async function uploadImage(imagePath) {
-  try {
-    const result = await cloudinary.uploader.upload(imagePath);
-    return result.url;
-  } catch (error) {
-    throw error;
-  }
-}
+// async function uploadImage(imagePath) {
+//   try {
+//     const result = await cloudinary.uploader.upload(imagePath);
+//     return result.url;
+//   } catch (error) {
+//     throw error;
+//   }
+// }
 
-module.exports = { uploadImage };
+// module.exports = { uploadImage };
