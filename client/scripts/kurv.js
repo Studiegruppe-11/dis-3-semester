8cc26919ad9226677f00eb3c20d1a0069f43fd89
gustav123ze <guze22ab@student.cbs.dk>
2023-12-03 12:20:22 +0100

    ret

--- client/scripts/kurv.js
+++ client/scripts/kurv.js
@@ -98,7 +98,7 @@ if (result && result.length > 0) {
         // Opdater teksten for at inkludere navn, pris og billede
         listItem.innerHTML = `<div>
                                  <img src="${product.imageUrl}" alt="${product.name}" style="width:50px;height:50px;">
-                                 <span> ${product.name}, ${product.price} kr. </span>
+                                 <span> class ="productName" ${product.name}, ${product.price} kr. </span>
                               </div>`;
 
         // Opret en knap for hvert produkt
