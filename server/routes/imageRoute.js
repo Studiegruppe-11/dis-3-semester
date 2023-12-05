d73a71b86f2aa0c2d9a30a5317cedfce73be71d9
Nicolas V. F. B. <nvfbeck@gmail.com>
2023-12-05 15:07:56 +0100

    Update image upload utility

--- server/routes/imageRoute.js
+++ server/routes/imageRoute.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const multer = require('multer');
 const router = express.Router();
-const { uploadImage } = require('../utility/cloudinaryUtility.js');
+const { uploadImage } = require('../utility/multipleImgUploadUtility.js');
 const fs = require('fs');
 const path = require('path');
 
