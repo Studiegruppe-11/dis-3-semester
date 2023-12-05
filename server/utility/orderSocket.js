8ef50e8965f7ba5a04e933c578f0b45c865d8e3c
gustav123ze <guze22ab@student.cbs.dk>
2023-12-05 17:00:20 +0100

    Update orderSocket.js

--- server/utility/orderSocket.js
+++ server/utility/orderSocket.js
@@ -1,64 +1,64 @@
-// root/server/utility/orderSocket.js
+// // root/server/utility/orderSocket.js
 
-const connection = require('../db/database1.js');
+// const connection = require('../db/database1.js');
 
-async function getPlacedOrders() {
-  try {
-    const pool = await connection.poolPromise;
+// async function getPlacedOrders() {
+//   try {
+//     const pool = await connection.poolPromise;
 
-    // Udfør SQL-forespørgslen for at hente ventende ordrer
-    const [rows] = await pool.query(`
-    SELECT placedorders_id, customers.first_name, products.name
-    FROM placedorders
-    INNER JOIN customers ON placedorders.customer_id = customers.customer_id
-    INNER JOIN products ON placedorders.product_id = products.product_id
-    WHERE placedorders.status = "waiting"
-        `);
+//     // Udfør SQL-forespørgslen for at hente ventende ordrer
+//     const [rows] = await pool.query(`
+//     SELECT placedorders_id, customers.first_name, products.name
+//     FROM placedorders
+//     INNER JOIN customers ON placedorders.customer_id = customers.customer_id
+//     INNER JOIN products ON placedorders.product_id = products.product_id
+//     WHERE placedorders.status = "waiting"
+//         `);
 
-    return rows;
-  } catch (error) {
-    console.error('Fejl under hentning af ventende ordrer:', error);
-    throw error;
-  }
-}
+//     return rows;
+//   } catch (error) {
+//     console.error('Fejl under hentning af ventende ordrer:', error);
+//     throw error;
+//   }
+// }
 
-const socketIO = require('socket.io');
+// const socketIO = require('socket.io');
 
-function setupOrderSocket(http) {
-  const io = socketIO(http).of('/order');
+// function setupOrderSocket(http) {
+//   const io = socketIO(http).of('/order');
 
-  io.on('connection', (socket) => {
-    console.log('En klient er tilsluttet via socket.');
+//   io.on('connection', (socket) => {
+//     console.log('En klient er tilsluttet via socket.');
     
-    // Lyt efter opdateringer i ventende ordrer
-    const emitPlacedOrders = async () => { 
-      try {
-        const placedOrders = await getPlacedOrders();
-        console.log('Placed orders updated:', placedOrders);
-        io.emit('placedOrdersUpdate', placedOrders);
-      } catch (error) {
-        console.error('Fejl under håndtering af opdatering af ventende ordrer:', error);
-      }
-    };
+//     // Lyt efter opdateringer i ventende ordrer
+//     const emitPlacedOrders = async () => { 
+//       try {
+//         const placedOrders = await getPlacedOrders();
+//         console.log('Placed orders updated:', placedOrders);
+//         io.emit('placedOrdersUpdate', placedOrders);
+//       } catch (error) {
+//         console.error('Fejl under håndtering af opdatering af ventende ordrer:', error);
+//       }
+//     };
 
-    // Udsend opdateringer ved forbindelse og derefter hver gang en bruger bestiller.
-    // setupOrderSocket importeres i order.route.js og bliver kaldt hver gang der bestilles.
-    emitPlacedOrders();
+//     // Udsend opdateringer ved forbindelse og derefter hver gang en bruger bestiller.
+//     // setupOrderSocket importeres i order.route.js og bliver kaldt hver gang der bestilles.
+//     emitPlacedOrders();
 
-    // Håndter 'getPlacedOrders' hændelsen fra klienten
-    socket.on('getPlacedOrders', async (callback) => {
-      try {
-        const placedOrders = await getPlacedOrders();
-        callback(placedOrders);
-      } catch (error) {
-        console.error('Fejl under hentning af ventende ordrer:', error);
-      }
-    });
+//     // Håndter 'getPlacedOrders' hændelsen fra klienten
+//     socket.on('getPlacedOrders', async (callback) => {
+//       try {
+//         const placedOrders = await getPlacedOrders();
+//         callback(placedOrders);
+//       } catch (error) {
+//         console.error('Fejl under hentning af ventende ordrer:', error);
+//       }
+//     });
 
-    socket.on('disconnect', () => {
-      console.log('En klient er frakoblet via socket.');
-    });
-  });
-}
+//     socket.on('disconnect', () => {
+//       console.log('En klient er frakoblet via socket.');
+//     });
+//   });
+// }
 
-module.exports = setupOrderSocket;
+// module.exports = setupOrderSocket;
