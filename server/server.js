7461716f2cd7485a52250c06374d93b054f2e6ab
gustav123ze <guze22ab@student.cbs.dk>
2023-12-06 14:36:49 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -101,8 +101,8 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   cookie: {
-      secure: false, // Set to true if using https
-      httpOnly: true,
+      secure: true, // Set to true if using https
+      httpOnly: false,
       maxAge: 1000 * 60 * 60 * 24 // 24 hours
   }
 }));
