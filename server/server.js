17da913a12c1c5819a3fdcd302ecc2ed0a3dfa9a
gustav123ze <guze22ab@student.cbs.dk>
2023-12-06 11:49:47 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -33,37 +33,45 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "../client")));
 
+
+
+
 // Favicon
 // app.use(favicon(path.join(__dirname, '../client/images', 'favicon.ico')));
 
-const RedisStore = require('connect-redis')(session);
+// const RedisStore = require('connect-redis')(session);
 
-const redisOptions = {
-  host: '164.90.228.42',
-  port: 6379,
-  // yderligere konfiguration efter behov
-};
+// const redisOptions = {
+//   host: '164.90.228.42',
+//   port: 6379,
+//   // yderligere konfiguration efter behov
+// };
 
-app.use(session({
-  store: new RedisStore(redisOptions),
-  secret: 'your-secret-key',
-  resave: false,
-  saveUninitialized: false,
-  cookie: {
-    secure: false,
-    httpOnly: true,
-    maxAge: 1000 * 60 * 60 * 24,
-  },
-}));
+// app.use(session({
+//   store: new RedisStore(redisOptions),
+//   secret: 'your-secret-key',
+//   resave: false,
+//   saveUninitialized: false,
+//   cookie: {
+//     secure: false,
+//     httpOnly: true,
+//     maxAge: 1000 * 60 * 60 * 24,
+//   },
+// }));
 
-// // til session storage
-// app.use(
-//   session({
-//     secret: "my-secret-key",
-//     resave: false,
-//     saveUninitialized: false,
-//   })
-// );
+
+
+
+
+
+// til session storage
+app.use(
+  session({
+    secret: "my-secret-key",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
 
 
