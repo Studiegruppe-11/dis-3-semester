ab0a65440f68168cc79eb587ac2ff6dd78289d0c
gustav123ze <guze22ab@student.cbs.dk>
2023-12-06 11:31:42 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -33,14 +33,35 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, "../client")));
 
 
-// til session storage
-app.use(
-  session({
-    secret: "my-secret-key",
-    resave: false,
-    saveUninitialized: false,
-  })
-);
+const RedisStore = require('connect-redis')(session);
+// Konfigurer Redis som sessionopbevaring
+const redisOptions = {
+  host: '164.90.228.42',
+  port: 6379, // standard Redis-port
+  // yderligere konfiguration efter behov
+};
+
+app.use(session({
+  store: new RedisStore(redisOptions),
+  secret: 'your-secret-key',
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    secure: false, // Sæt til true, hvis du bruger HTTPS
+    httpOnly: true,
+    maxAge: 1000 * 60 * 60 * 24, // Sessionens levetid i millisekunder (her 1 dag)
+  },
+}));
+
+
+// // til session storage
+// app.use(
+//   session({
+//     secret: "my-secret-key",
+//     resave: false,
+//     saveUninitialized: false,
+//   })
+// );
 
 
 
