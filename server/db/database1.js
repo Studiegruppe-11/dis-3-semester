3378e1a7f7f2030f0f488291e1c3d508fa033281
Nicolas V. F. B <nvfbeck@gmail.com>
2023-12-12 13:58:53 +0100

    Update database1.js

--- server/db/database1.js
+++ server/db/database1.js
@@ -1,6 +1,16 @@
 // root/server/db/database1.js
 // dette er den der bruges
 
+// Ansvarlig for at oprette og eksportere en forbindelsespool til din MySQL-database ved hjælp af mysql2/promise biblioteket. Den bruger miljøvariabler til at konfigurere forbindelsen, som er defineret i .env filen i roden af dit projekt.
+// require statements: Importerer nødvendige moduler. path og dotenv bruges til at håndtere miljøvariabler, og mysql2/promise bruges til at oprette en forbindelse til MySQL-databasen.
+// dotenv.config: Indlæser miljøvariabler fra lokal .env fil.
+// dbConfig: Definerer konfigurationen for databasen ved hjælp af miljøvariabler.
+// pool: Opretter en forbindelsespool ved hjælp af dbconfig konfigurationen.
+// module.exports: Eksporterer forbindelsespoolen, så den kan genbruges i andre dele af applikationen.
+// Brugt i andre filer i server/ mappen, specifikt i filer, der har brug for at interagere med databasen. 
+// For eksempel, i routes/ mappe, er der filer, der bruger denne forbindelsespool til at udføre SQL-forespørgsler.
+
+
 const path = require('path');
 const dotenv = require('dotenv');
 const mysql = require('mysql2/promise');
