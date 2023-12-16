90ea3380f3715c136fcd9fb745929522ba635571
Nicolas V. F. B <nvfbeck@gmail.com>
2023-12-16 21:45:56 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -54,7 +54,7 @@ app.use(express.static(path.join(__dirname, "../client")));
 // Redis session storage (local host)
 
 const redis = require('redis');
-const RedisStore = require("connect-redis")(session)
+const RedisStore = require("connect-redis").default(session)
 
 // Create Redis Client
 const redisClient = redis.createClient({
