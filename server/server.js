79876a26089b35ce6c7d896ba738c6c429f29a47
gustav123ze <guze22ab@student.cbs.dk>
2023-12-20 12:09:21 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -145,7 +145,6 @@ app.post('/smstext', (req, res) => {
     try {
       const response = await fetch("/totalRevenue");
       const result = await response.json();
-      console.log(result.total_price);
       if (result.total_price) {
    
         twiml.message('Dagens omsætning er: ' + result.total_price + ' kr.');
